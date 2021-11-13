3a257f6626007c6c59aa84cd97ae8b9c412d00c4
Mitesh Upadhyay <upadhyaymitesh91@gmail.com>
2021-11-13 08:08:15 +0530

    First Commit

---
+++ src/test/java/com/mitesh/tech/learn/bankservice/BankserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitesh.tech.learn.bankservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
